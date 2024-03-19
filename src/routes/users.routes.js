d9587ad1498a576a7030baee355ae1e1ce101775
Mateus Gabriel dos S.S. <mgsslayla@gmail.com>
2024-03-19 19:06:33 -0300

    salvando arquivo no bd

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,14 +4,17 @@ const uploadConfig = require("../configs/upload");
 
 const UsersController = require("../controllers/UsersController");
 const ensureAuthenticated = require("../middleware/ensureAuthenticatd");
+const UserAvatarController = require("../controllers/UserAvatarController");
 
 const usersRoutes = Router();
 const upload = multer(uploadConfig.MULTER);
 
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/", ensureAuthenticated, usersController.update);
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), userAvatarController.update);
 
 usersRoutes.patch("/avatar", 
                     ensureAuthenticated,
